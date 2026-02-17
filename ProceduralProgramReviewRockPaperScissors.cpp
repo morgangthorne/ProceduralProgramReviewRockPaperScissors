d4c802804343b6b33b32a27c12316d06e869604f
morgan thorne <morgangthorne@outlook.com>
2026-02-17 17:12:02 -0500

    Created all function prototypes and starting to flesh them out.

--- ProceduralProgramReviewRockPaperScissors.cpp
+++ ProceduralProgramReviewRockPaperScissors.cpp
@@ -13,16 +13,22 @@ using namespace std;
 
 int GetRandomNumber();
 
-int RockPaperScissors();
+int RockPaperScissorsValidator();
 
 void GameDisplay();
 
 void WinLossDrawTracker();
 
+void ReplayQuit();
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    GameDisplay();
+
+    RockPaperScissorsValidator();
+
+    WinLossDrawTracker();
 }
 
 //Creates a random number from 1-3 to simulate rock, paper, scissors
@@ -36,7 +42,50 @@ int GetRandomNumber() {
 }
 
 
+//Function Displays directions for the game
+void GameDisplay() {
+
+}
 
 
 
 
+
+
+
+//Function validates user's inputs and determines the winner of the game, then result is sent to WinLossDrawTracker
+int RockPaperScissorsValidator(int ComputerChoice) {
+    
+}
+
+
+
+//Function receives value from RockPaperScissor function and will increment win,loss, and draw values. Those values will be displayed
+//at the end of the game to the user. 
+void WinLossDrawTracker() {
+    int Wins, 
+        Losses, 
+        Draws = 0;
+    
+
+
+
+}
+
+//Function will let user choose to keep playing or quit
+void ReplayQuit() {
+    string DoAgain;
+
+    do {
+
+        int Number = GetRandomNumber();
+
+        int GameResult = RockPaperScissorsValidator(Number);
+
+
+    } while (DoAgain == "Yes" || DoAgain == "yes");
+    cout << "Thank you for playing!\n";
+
+
+
+}
