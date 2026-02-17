c04e9833216a51e54921c542fa31b3790d1e89ed
morgan thorne <morgangthorne@outlook.com>
2026-02-17 16:42:45 -0500

    Add project files.

---
+++ ProceduralProgramReviewRockPaperScissors.cpp
@@ -0,0 +1,41 @@
+/* 
+FileName: ProceduralProgramReviewRockPaperScissors.cpp
+Programmer: Morgan Thorne
+Date: February 2026
+Program: Create a modular program using the <random> library. The program will allow the user to play against the computer in a game of rock, paper, scissors
+, wins, losses, and draws will be tracked until the user decides to quit/stop the game.
+*/
+
+#include <iostream>
+#include <random>
+
+using namespace std;
+
+int GetRandomNumber();
+
+int RockPaperScissors();
+
+void GameDisplay();
+
+void WinLossDrawTracker();
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+//Creates a random number from 1-3 to simulate rock, paper, scissors
+int GetRandomNumber() {
+    random_device x;
+    uniform_int_distribution<int> randomInt(1, 3);
+
+    int Number = randomInt(x);
+
+    return Number;
+}
+
+
+
+
+
