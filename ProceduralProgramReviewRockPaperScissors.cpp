31ef3b960511fb832f61d81db996432cae59ec08
morgan thorne <morgangthorne@outlook.com>
2026-02-17 21:14:55 -0500

    Fleshing out functions, getting their basic logic down before passing values between them.

--- ProceduralProgramReviewRockPaperScissors.cpp
+++ ProceduralProgramReviewRockPaperScissors.cpp
@@ -12,23 +12,18 @@ Program: Create a modular program using the <random> library. The program will a
 using namespace std;
 
 int GetRandomNumber();
-
-int RockPaperScissorsValidator();
-
+int GetUserChoice();
+int DetermineOutcome();
 void GameDisplay();
-
-void WinLossDrawTracker();
-
 void ReplayQuit();
-
 
 int main()
 {
     GameDisplay();
 
-    RockPaperScissorsValidator();
+    GetUserChoice();
 
-    WinLossDrawTracker();
+    DetermineOutcome();
 }
 
 //Creates a random number from 1-3 to simulate rock, paper, scissors
@@ -44,17 +39,27 @@ int GetRandomNumber() {
 
 //Function Displays directions for the game
 void GameDisplay() {
+    cout << "---------------------------------------------\n";
 
+    cout << "Choose Rock (1), Paper (2), or Scissors(3): ";
 }
 
 
+//Function validates user's input then result is sent to WinLossDrawValidator
+int GetUserChoice() {
+    int UserChoice;
 
+    cin >> UserChoice;
 
-
-
-
-//Function validates user's inputs and determines the winner of the game, then result is sent to WinLossDrawTracker
-int RockPaperScissorsValidator(int ComputerChoice) {
+    do {
+        cin >> UserChoice;
+        
+        if (UserChoice < 1 || UserChoice > 3) {
+            cout << "Please choose a number from 1 - 3: ";
+        }
+    } while (UserChoice > 1 || UserChoice < 3);
+        
+    
     
 }
 
@@ -62,14 +67,31 @@ int RockPaperScissorsValidator(int ComputerChoice) {
 
 //Function receives value from RockPaperScissor function and will increment win,loss, and draw values. Those values will be displayed
 //at the end of the game to the user. 
-void WinLossDrawTracker() {
-    int Wins, 
-        Losses, 
+int DetermineOutcome(int UserChoice, int ComputerChoice) {
+    int Wins = 0,
+        Losses = 0,
         Draws = 0;
+
+    if (UserChoice == ComputerChoice) {
+        cout << "Draw\n";
+        Draws++;
+        
+        return Draws;
+    }
+    else if ((UserChoice == 1 && ComputerChoice == 3) || (UserChoice == 2 && ComputerChoice == 1) || (UserChoice == 3 && ComputerChoice == 2)) {
+        cout << "Win\n";
+        Wins++;
+
+        return Wins;
+    }
+    else {
+        cout << "Loss\n";
+        Losses++;
+
+        return Losses;
+    }
     
-
-
-
+    return 0;
 }
 
 //Function will let user choose to keep playing or quit
@@ -80,12 +102,10 @@ void ReplayQuit() {
 
         int Number = GetRandomNumber();
 
-        int GameResult = RockPaperScissorsValidator(Number);
+        //int GameResult = RockPaperScissorsValidator(Number);
 
 
     } while (DoAgain == "Yes" || DoAgain == "yes");
     cout << "Thank you for playing!\n";
-
-
 
 }
